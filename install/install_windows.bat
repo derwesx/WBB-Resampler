641d0a15edf5d200c1bd202d0b8abf182090779a
Derwes <64682465+derwesx@users.noreply.github.com>
2025-03-06 16:41:50 +0300

    Just install python...

--- install/install_windows.bat
+++ install/install_windows.bat
@@ -1,20 +1,5 @@
 @echo off
 
-:: Check if Python is already installed
-where python >nul 2>nul
-if %errorlevel% equ 0 (
-    echo Python is already installed.
-    goto :install_dependencies
-)
-
-:: Download and install Python
-echo Downloading Python installer...
-powershell -command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.11.3/python-3.11.3.exe', 'C:/Tools/python-3.11.3.exe'); & c:\Tools\python-3.11.3.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=c:\Tools\Python311; [Environment]::SetEnvironmentVariable('PATH', ${env:path} + ';C:\Tools\Python311', 'Machine')"
-
-:: Refresh environment variables
-setx PATH "%PATH%" >nul
-
-:install_dependencies
 :: Check if pip is installed
 python -m ensurepip --upgrade
 
